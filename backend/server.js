48c2fe00ffb01a835d71db0d625344f5648c6e02
S4hn1p <pinhas.roumani@gmail.com>
2025-07-30 11:42:10 +0200

    Missing socket work

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from "path"
 import dotenv from "dotenv"
 import express from "express"
 import cookieParser from "cookie-parser"
@@ -14,6 +15,8 @@ import { app, server } from './socket/socket.js'
 const PORT = 3000
 // process.env.PORT
 
+const __dirname = path.resolve()
+
 dotenv.config()
 
 app.use(express.json())
@@ -29,6 +32,11 @@ app.use('/api/auth', authRoutes)
 app.use('/api/messages', messageRoutes)
 app.use('/api/users', userRoutes)
 
+app.use(express.static(path.join(__dirname, "/frontend/dist")))
+
+app.get("/*splat", (req, res) => {
+    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"))
+})
 
 server.listen(PORT, () => {
     connectToMongoDB();
